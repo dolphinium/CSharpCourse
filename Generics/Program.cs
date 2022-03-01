3673a998e38d3d62c77f7501f8affb343b720207
dolphinium <dolphofficial@gmail.com>
2022-03-02 01:32:26 +0300

    Generics and Generic Repository Rules.

---
+++ Generics/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            List<string> result = utilities.BuildList<string>("Ankara","İzmir","Adana");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);   
+            }
+
+            List<Customer> result2 = utilities.BuildList<Customer>(new Customer
+            {
+                FirstName = "Yunus"
+            },
+            new Customer
+            {
+                FirstName = "Emre"
+            });
+
+            foreach (var customer in result2)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Utilities
+    {
+        public List<T> BuildList<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+    }
+
+    class Product
+    {
+    }
+
+    class Customer
+    {
+        public string FirstName { get; set; }
+    }
+
+    interface ICustomerDal:IRepository<Customer>
+    {
+    }
+
+    interface IProductDal:IRepository<Product>
+    {
+    
+    }
+
+    // Generic Repository Example
+    interface IRepository<T>  where T:class, new ()  // to restrict of a generics obtained by this.--> where T: "class", new()
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+
+    class ProductDal:IProductDal
+    {
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CustomerDal:ICustomerDal
+    {
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
